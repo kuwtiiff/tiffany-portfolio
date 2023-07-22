7300907fdf8e9cd28943125806671a4beaadc019
kuwtiiff <131501131+kuwtiiff@users.noreply.github.com>
2023-07-21 23:19:03 -0700

    added components and nav bar

---
+++ src/components/AboutPage/AboutPage.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import './AboutPage.css';
+const AboutPage= () => {
+    
+
+    return(
+ <div className = "about-container">
+    <h1>
+        About Me
+    </h1>
+        </div>
+    )
+       
+    
+};
+
+export default AboutPage;
---
+++ src/components/DesignPage/DesignPage.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import './DesignPage.css';
+const DesignPage= () => {
+    
+
+    return(
+ <div className = "design-container">
+    <h1>
+       Designs
+    </h1>
+        </div>
+    )
+       
+    
+};
+
+export default DesignPage;
