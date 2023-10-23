d8540a83fff94c2d6c30fb696fb1efa730b502ca
kuwtiiff <131501131+kuwtiiff@users.noreply.github.com>
2023-10-22 18:44:53 -0700

    added sizzle widget in homepage

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import './HomePage.css';
 import {useNavigate} from 'react-router-dom';
-import pic1 from './dscicon.png'
-import pic2 from './BobecoHeartsKetleLogo.png'
+import pic1 from './dscicon.png';
+import pic2 from './BobecoHeartsKetleLogo.png';
+import pic4 from './s_sizzlelogo.png';
 
 const HomePage= () => {
     const navigate = useNavigate();
@@ -101,8 +102,30 @@ const HomePage= () => {
                 <div className="google-logo">
                     <img src={pic1} alt="DSC Logo" />
                 </div>
+            </div>  
+
+            <div className="sizzle-container" onClick={() => handleClick('/sizzle-app')}>
+            <div className="sizzle-heading">
+                <h1>
+                    SIZZLE MOBILE/WEB APP
+                </h1>
+                <div className="sizzle-logo">
+                    <img src={pic4} alt="Sizzle S" />
+                </div>
+            </div>
+            <div className="sizzle-desc">
+                <h2>
+                    UX/UI Designer | Dietician-Patient Web & Mobile App
+                </h2>
+            </div>
+            <div className="sizzle-intro">
+                <h2>
+                    I am delighted to currently be a part of Sizzle, a pioneering startup dedicated to transforming the interaction between dieticians and patients. My role involves crafting intuitive user experiences for our web and mobile applications, to empower both users to navigate their healthcare journeys effortlessly.
+                </h2>
             </div>          
-        </div>
+        </div>  
+
+        </div>   
 
     ) 
 };
